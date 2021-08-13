6c70a971fe327e228e092a8e7036daa42ce1a0b9
Mack Maier <hmaiermack@gmail.com>
2021-08-13 09:30:48 -0700

    RegisterPage react-hook-form and yup integration

---
+++ client/src/components/FormInput.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IFormInputProps {
+
+}
+
+const FormInput = (props: IFormInputProps) => {
+
+  return (
+    <></>
+  );
+};
+
+export default FormInput;
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup'
+import YupPassword from 'yup-password'
+//extend yup
+YupPassword(yup)
+
+interface IFormInputs {
+    username: string,
+    email: string,
+    password: string,
+    confirmation: string
+}
+
+//need to add existing email validation
+
+const registerSchema = yup.object().shape({
+    username: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().password().required(),
+    confirmation: yup.string().password().oneOf([yup.ref("password"), 'Passwords must match.'])
+})
 
 const RegisterPage = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm<IFormInputs>({
+        resolver: yupResolver(registerSchema)
+    });
+
+    const submitHandler: SubmitHandler<IFormInputs> = (input: IFormInputs) => {
+        console.log("submit");
+    }
+
     return (
         <div className="container">
             <div className="min-h-screen bg-gray-100 flex flex-col justify-center">
@@ -9,25 +38,29 @@ const RegisterPage = () => {
                     <div className="text-xl font-medium text-pGrey-400 m-2 text-center">Register</div>
                 </div>
                 <div className="max-w-md w-full mx-auto bg-white p-8 border border-pGrey-600 rounded">
-                    <form action="" className="space-y-6">
+                    <form onSubmit={handleSubmit(data => console.log(data))} className="space-y-6">
                         <div>
                             <label htmlFor="" className="text-sm font-bold text-gray-600 block">Username</label>
-                            <input type="text" className="w-full p-2 border border-pGrey-600 rounded mt-1" />
+                            <input {...register("username")} type="text" className="w-full p-2 border border-pGrey-600 rounded mt-1" />
+                            {errors.username?.type === 'required' && "Username is required"}
                         </div>
                         <div>
                             <label htmlFor="" className="text-sm font-bold text-gray-600 block">Email</label>
-                            <input type="text" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="jdoe@example.com" />
+                            <input {...register("email")} type="text" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="jdoe@example.com" />
+                            {errors.email && errors.email?.message && <span>{errors.email.message}</span>}
                         </div>
                         <div>
                             <label htmlFor="" className="text-sm font-bold text-gray-600 block">Password</label>
-                            <input type="password" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="********"/>
+                            <input {...register("password")} type="password" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="********"/>
+                            {errors.password && errors.password?.message && <span>{errors.password.message}</span>}
                         </div>
                         <div>
                             <label htmlFor="" className="text-sm font-bold text-gray-600 block">Confirm Password</label>
-                            <input type="password" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="********"/>
+                            <input {...register("confirmation")} type="password" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="********"/>
+                            {errors.confirmation && errors.confirmation?.message && <span>Passwords must match.</span>}
                         </div>
 
-                        <button className="w-full py-2 bg-pBlue-400 hover:bg-pBlue-600 rounded-md text-pGrey-400 text-lg font-bold">Register</button>
+                        <input type="submit" className="w-full py-2 bg-pBlue-400 hover:bg-pBlue-600 rounded-md text-pGrey-400 text-lg font-bold"></input>
                     </form>
                 </div>
             </div>
