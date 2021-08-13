276a2579b2cadb7cf7eecacff831c830e9cf46e7
Mack Maier <hmaiermack@gmail.com>
2021-08-13 10:56:37 -0700

    new FormInput component with error handling

--- client/src/components/FormInput.tsx
+++ client/src/components/FormInput.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
+import { string } from 'yup/lib/locale';
 
 interface IFormInputProps {
-
+    children: React.ReactNode;
+    label: string;
+    required?: boolean
+    register: any;
+    errors?: any;
 }
 
-const FormInput = (props: IFormInputProps) => {
+//probably naive implementation -> look into doing it better
+const FormInput = ({children, register, label, required, errors}: IFormInputProps) => {
 
   return (
-    <></>
+    <div>
+        <label className={errors ? "text-sm font-bold text-red-600 block" : "text-sm font-bold text-gray-600 block"}>{label}</label>
+        <input type="text" {...register(label, {required: required})} className={errors ? "w-full p-2 border-2 border-red-600 rounded mt-1" : "w-full p-2 border border-gray-600 rounded mt-1" } />
+        {errors && (<span className="text-red-600">
+            {children}
+            </span>)}
+    </div>
   );
 };
 
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -2,22 +2,23 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup'
 import YupPassword from 'yup-password'
+import FormInput from '../components/FormInput';
 //extend yup
 YupPassword(yup)
 
 interface IFormInputs {
-    username: string,
-    email: string,
-    password: string,
-    confirmation: string
+    Username: string,
+    Email: string,
+    Password: string,
+    Confirmation: string
 }
 
 //need to add existing email validation
 
 const registerSchema = yup.object().shape({
-    username: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().password().required(),
+    Username: yup.string().required(),
+    Email: yup.string().email().required(),
+    Password: yup.string().password().required(),
     confirmation: yup.string().password().oneOf([yup.ref("password"), 'Passwords must match.'])
 })
 
@@ -39,26 +40,18 @@ const RegisterPage = () => {
                 </div>
                 <div className="max-w-md w-full mx-auto bg-white p-8 border border-pGrey-600 rounded">
                     <form onSubmit={handleSubmit(data => console.log(data))} className="space-y-6">
-                        <div>
-                            <label htmlFor="" className="text-sm font-bold text-gray-600 block">Username</label>
-                            <input {...register("username")} type="text" className="w-full p-2 border border-pGrey-600 rounded mt-1" />
-                            {errors.username?.type === 'required' && "Username is required"}
-                        </div>
-                        <div>
-                            <label htmlFor="" className="text-sm font-bold text-gray-600 block">Email</label>
-                            <input {...register("email")} type="text" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="jdoe@example.com" />
-                            {errors.email && errors.email?.message && <span>{errors.email.message}</span>}
-                        </div>
-                        <div>
-                            <label htmlFor="" className="text-sm font-bold text-gray-600 block">Password</label>
-                            <input {...register("password")} type="password" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="********"/>
-                            {errors.password && errors.password?.message && <span>{errors.password.message}</span>}
-                        </div>
-                        <div>
-                            <label htmlFor="" className="text-sm font-bold text-gray-600 block">Confirm Password</label>
-                            <input {...register("confirmation")} type="password" className="w-full p-2 border border-pGrey-600 rounded mt-1" placeholder="********"/>
-                            {errors.confirmation && errors.confirmation?.message && <span>Passwords must match.</span>}
-                        </div>
+                        <FormInput label="Username" register={register} errors={errors.Username} required={true}>
+                            {errors.Username && errors.Username?.message && <span>{errors.Username.message}</span>}
+                        </FormInput>
+                        <FormInput label="Email" register={register} errors={errors.Email} required={true}>
+                            {errors.Email && errors.Email?.message && <span>{errors.Email.message}</span>}
+                        </FormInput>
+                        <FormInput label="Password" register={register} errors={errors.Password} required={true}>
+                            {errors.Password && errors.Password?.message && <span>{errors.Password.message}</span>}
+                        </FormInput>
+                        <FormInput label="Confirmation" register={register} errors={errors.Confirmation} required={true}>
+                            {errors.Confirmation && errors.Confirmation?.message && <span>{errors.Confirmation.message}</span>}
+                        </FormInput>
 
                         <input type="submit" className="w-full py-2 bg-pBlue-400 hover:bg-pBlue-600 rounded-md text-pGrey-400 text-lg font-bold"></input>
                     </form>
